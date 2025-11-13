fc9cc7b767c06e0dae2df4162f48688bfdb6d550
Vann Xyn Pok <pok_vann_xyn@s2022.ssts.edu.sg>
2025-11-13 12:38:57 +0800

    Finished v1 of code

---
+++ run.sh
@@ -0,0 +1,18 @@
+echo "Starting..."
+cd /home/mgrm1/mgrm1_cv_link
+echo "Navigated to CV"
+echo "Activating cv..."
+source venv/bin/activate
+python cv.py &
+CV_LINK=$!
+echo "CV activated"
+deactivate
+
+echo "Activating UI..."
+flutter-pi /home/flutter_assets/
+FLUTTER_UI=$!
+echo "Activated UI"
+deactivate
+
+trap "kill $CV_LINK $FLUTTER_UI" SIGINT
+wait
